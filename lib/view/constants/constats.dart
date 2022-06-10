6388d174f7c1658ea0d7b639e347cc3b6542a216
jopaul23 <jopauljoy999@gmail.com>
2022-06-10 21:07:57 +0530

    structure creted

---
+++ lib/view/constants/constats.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+const kDefaultPadding = 16.0, kBorderRadius = 12.0;
+
+const kButtonGradient = LinearGradient(
+    begin: Alignment.topRight,
+    end: Alignment.bottomLeft,
+    colors: [Color(0xff8AD061), Color(0xff5CC369)]);
+const kGreenSecondary = Color(0xff5CC369);
+const kGreen = Color(0xff5FBD00);
+
+const kShadowColor = Colors.black26;
+
+const kErrorRed = Color(0xFFff9292);
+
+const kWhite = Color(0xffFAFAFA);
+
+const kSecondaryButtonColor = Color(0xffEEFBED);
+
+const kHintTextColor = Color(0xffCCCCCC);
+
+const kIconBgColor = Color(0xffE9E9E9);
+
+const kItemContainerBg = Color(0xffF1F1F1);
+
+const kPrimaryTextColor = Color(0xff37474F);
+
+const kGrey = Color(0xff888888);
+
+const kFormAlternateGreen = Color(0xfff3f9f2);
+
+const kLightGrey = Color(0xffF1F1F1);
+
+const kBgColor = Color(0xffFBFAFF);
